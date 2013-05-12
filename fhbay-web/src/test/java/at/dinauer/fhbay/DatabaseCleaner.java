997c29ed6a1b14adbb92802feb90a231e3e38d9a
Alexander Dinauer <alexander@dinauer.at>
2013-05-12 20:39:16 +0200

    add util class for cleaning the database before each test

---
+++ fhbay-web/src/test/java/at/dinauer/fhbay/DatabaseCleaner.java
@@ -0,0 +1,64 @@
+package at.dinauer.fhbay;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseCleaner {
+	private static final String[] TABLES_TO_CLEAN = {
+		"Address",
+		"Article",
+		"Category",
+		"Customer",
+		"PaymentData",
+		"Phone"
+	};
+	
+	private Connection connection;
+
+	public void clean() throws Exception {
+		prepareForCleaning();
+        
+        cleanTables();
+        
+        afterCleaning();
+	}
+
+	private void cleanTables() throws SQLException {
+		for (String table : TABLES_TO_CLEAN) {
+			connection.createStatement().execute("TRUNCATE TABLE " + table);
+		}
+	}
+
+	private void prepareForCleaning() throws ClassNotFoundException,
+			SQLException {
+		loadDriver();
+        openConnection();
+        disableForeignKeyChecks();
+	}
+
+	private void afterCleaning() throws SQLException {
+		enableForeignKeyChecks();
+        closeConnection();
+	}
+
+	private void loadDriver() throws ClassNotFoundException {
+		Class.forName("com.mysql.jdbc.Driver");
+	}
+
+	private void openConnection() throws ClassNotFoundException, SQLException {
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:13306/fhbay", "fhbay", "pwd");
+	}
+	
+	private void disableForeignKeyChecks() throws SQLException {
+		connection.createStatement().execute("SET FOREIGN_KEY_CHECKS=0");
+	}
+
+	private void enableForeignKeyChecks() throws SQLException {
+		connection.createStatement().execute("SET FOREIGN_KEY_CHECKS=1");
+	}
+
+	private void closeConnection() throws SQLException {
+		connection.close();
+	}
+}
